5975c8fc959b8c815e7b89de4d7a5f69ad4a73e9
kuppaDileepkumar <135305572+kuppaDileepkumar@users.noreply.github.com>
2025-05-05 09:20:27 +0530

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('./User');
 
 const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
